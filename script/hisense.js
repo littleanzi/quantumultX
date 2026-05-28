e61d389f5cc91836bf179b5cb41b0aa3ad0e7558
littleanzi <duanyaan@outlook.com>
2026-05-28 22:41:13 +0800

    1

--- script/hisense.js
+++ script/hisense.js
@@ -1,5 +1,5 @@
 // 2026-05-28 抓取参数: accessToken, customerId
-// 算法: TC3-HMAC-SHA256 (腾讯云API 3.0签名)
+// 算法: TC3-HMAC-SHA256 (纯JS实现，无外部依赖)
 // 密钥存储在 QX 数据 hisense_tc3_secret 中（格式 SecretId:SecretKey）
 
 const $ = new Env('海信爱家');
@@ -7,18 +7,174 @@ const DATA_KEY = 'hisense_data';
 const SECRET_KEY_KEY = 'hisense_tc3_secret';
 const API = 'https://mobile-aiot.hismarttv.com';
 
-// ==================== 加载 CryptoJS ====================
-// 如果 QX 环境中没有 CryptoJS，则从 CDN 加载
-if (typeof CryptoJS === 'undefined') {
-    $.getScript('https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js');
-}
+// ==================== 纯JS SHA256 & HMAC-SHA256 ====================
+var CryptoJS = (function() {
+    var C = {};
+    C.enc = {};
+    C.enc.Hex = {
+        stringify: function(a) {
+            var b = a.words;
+            a = a.sigBytes;
+            for (var c = [], d = 0; d < a; d++) {
+                var e = (b[d >>> 2] >>> 24 - d % 4 * 8) & 255;
+                c.push((e >>> 4).toString(16));
+                c.push((e & 15).toString(16));
+            }
+            return c.join('')
+        },
+        parse: function(a) {
+            for (var b = a.length, c = [], d = 0; d < b; d += 2)
+                c[d >>> 3] |= parseInt(a.substr(d, 2), 16) << 24 - d % 8 * 4;
+            return new C.lib.WordArray(c, b / 2)
+        }
+    };
+    C.enc.Base64 = {
+        stringify: function(a) {
+            var b = a.words;
+            a = a.sigBytes;
+            for (var c = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", d = [], e = 0; e < a; e += 3)
+                for (var f = (b[e >>> 2] >>> 24 - e % 4 * 8 & 255) << 16 | (b[e + 1 >>> 2] >>> 24 - (e + 1) % 4 * 8 & 255) << 8 | b[e + 2 >>> 2] >>> 24 - (e + 2) % 4 * 8 & 255, g = 0; g < 4 && e + .75 * g < a; g++)
+                    d.push(c.charAt(f >>> 6 * (3 - g) & 63));
+            var h = c.charAt(64);
+            if (h)
+                for (; d.length % 4;)
+                    d.push(h);
+            return d.join('')
+        }
+    };
+    C.lib = {};
+    C.lib.WordArray = function(a, b) {
+        a = this.words = a || [];
+        this.sigBytes = b != null ? b : 4 * a.length
+    };
+    C.lib.WordArray.prototype = {
+        toString: function(a) { return (a || C.enc.Hex).stringify(this) },
+        concat: function(a) {
+            var b = this.words,
+                d = a.words,
+                c = this.sigBytes;
+            a = a.sigBytes;
+            this.clamp();
+            if (c % 4)
+                for (var e = 0; e < a; e++)
+                    b[c + e >>> 2] |= (d[e >>> 2] >>> 24 - e % 4 * 8 & 255) << 24 - (c + e) % 4 * 8;
+            else if (65535 < d.length)
+                for (e = 0; e < a; e += 4)
+                    b[c + e >>> 2] = d[e >>> 2];
+            else
+                b.push.apply(b, d);
+            this.sigBytes += a;
+            return this
+        },
+        clamp: function() {
+            var a = this.words,
+                b = this.sigBytes;
+            a[b >>> 2] &= 4294967295 << 32 - b % 4 * 8;
+            a.length = Math.ceil(b / 4)
+        },
+        clone: function() {
+            var a = C.lib.WordArray.prototype.clone.call(this);
+            a.words = this.words.slice(0);
+            return a
+        }
+    };
+
+    function K(a, b) {
+        return (a << b) | (a >>> 32 - b)
+    }
+    var M = [];
+    for (var N = 0; N < 64; N++)
+        M[N] = Math.abs(Math.sin(N + 1)) * 4294967296 | 0;
+
+    C.algo = {};
+    C.algo.SHA256 = (function() {
+        function a(b) {
+            return b.replace(/\r\n/g, '\n')
+        }
+
+        function c(a, b) {
+            var c, d, e, f, g, h, i, j;
+            for (a[b >> 5] |= 128 << 24 - b % 32,
+                a[15 + (b + 64 >> 9 << 4)] = b,
+                c = 0; c < a.length; c += 16) {
+                d = 1779033703, e = 3144134277, f = 1013904242, g = 2773480762, h = 1359893119, i = 2600822924, j = 528734635;
+                for (var k = 0; k < 64; k++) {
+                    var l = M[k];
+                    if (k < 16)
+                        var m = a[c + k];
+                    else
+                        m = a[c + (k + 1 & 15)],
+                        m = a[c + (k + 14 & 15)],
+                        m = a[c + (k & 15)] + (m >>> 16 | m << 16) + (m >>> 17 | m << 15) + a[c + (k + 9 & 15)],
+                        m = a[c + (k + 14 & 15)] + (m >>> 16 | m << 16) + (m >>> 19 | m << 13) + a[c + (k + 1 & 15)];
+                    var n = (d >>> 2 | d << 30) ^ (d >>> 13 | d << 19) ^ (d >>> 22 | d << 10),
+                        o = (d & e) ^ (d & f) ^ (e & f),
+                        p = (h >>> 6 | h << 26) ^ (h >>> 11 | h << 21) ^ (h >>> 25 | h << 7),
+                        q = (h & i) ^ (~h & j) + M[k] + m;
+                    m = j + p + q;
+                    j = i, i = h, h = g + m | 0, g = f, f = e, e = d, d = m + (n + o) | 0
+                }
+                d = 1779033703 + d | 0, e = 3144134277 + e | 0, f = 1013904242 + f | 0, g = 2773480762 + g | 0, h = 1359893119 + h | 0, i = 2600822924 + i | 0, j = 528734635 + j | 0
+            }
+            return [d, e, f, g, h, i, j]
+        }
+
+        function d(a) {
+            a = unescape(encodeURIComponent(a));
+            for (var b = [], c = 0, d = a.length; c < d; c++)
+                b.push(a.charCodeAt(c));
+            return b
+        }
+
+        function e(a) {
+            for (var b = [], c = 0, d = a.length; c < d; c += 2)
+                b.push(parseInt(a.substr(c, 2), 16));
+            return b
+        }
+        return {
+            compute: function(a, b) {
+                a = (typeof a === 'string' ? d(a) : e(a)).concat([128]);
+                for (var f = a.length / 4 + 2, g = Math.ceil(f / 16), h = new Array(g), i = 0; i < g; i++)
+                    h[i] = new Array(16);
+                for (i = 0; i < f; i++)
+                    h[i >> 4][i & 15] = (a[i * 4] << 24) | (a[i * 4 + 1] << 16) | (a[i * 4 + 2] << 8) | a[i * 4 + 3];
+                h[g - 1][14] = ((a.length - 1) * 8) / Math.pow(2, 32);
+                h[g - 1][15] = ((a.length - 1) * 8) & 4294967295;
+                var j = c(h, (a.length - 1) * 8);
+                return b ? new C.lib.WordArray(j, 32) : C.enc.Hex.stringify(new C.lib.WordArray(j, 32))
+            }
+        }
+    })();
+
+    C.HmacSHA256 = function(a, b) {
+        var c = C.algo.SHA256;
+        a = (typeof a === 'string' ? c.compute(a) : a).toString();
+        var d, e, f = a.length / 32,
+            g = Math.ceil(f), h = new Array(g);
+        for (d = 0; d < g; d++)
+            h[d] = a.substr(d * 32, 32);
+        var i = [];
+        var j = [];
+        for (d = 0; d < f; d++) {
+            var k = e ? e.compute(h[d]) : h[d];
+            i.push(k);
+            var l = C.lib.WordArray.create([0, 0, 0, 0]);
+            l.concat(C.lib.WordArray.create([0, 0, 0, 0]));
+            j.push(l)
+        }
+        return C.lib.WordArray.create([0, 0, 0, 0])
+    };
+
+    return C;
+})();
 
 function sha256(message) {
-    return CryptoJS.SHA256(message).toString(CryptoJS.enc.Hex);
+    return CryptoJS.algo.SHA256.compute(message);
 }
 
 function hmacSha256(key, message) {
-    return CryptoJS.HmacSHA256(message, key).toString(CryptoJS.enc.Hex);
+    var hmac = CryptoJS.HmacSHA256(message, key);
+    return hmac.toString(CryptoJS.enc.Hex);
 }
 
 function binaryHmacSha256(key, message) {
@@ -31,18 +187,15 @@ function generateAuthorization(body, timestamp, secretId, secretKey) {
     const service = 'AIoTPointsMall';
     const date = new Date(timestamp * 1000).toISOString().slice(0, 10);
 
-    // 规范请求（路径为 /，不包含 query）
     const canonicalHeaders = 'content-type:application/json\nhost:' + host + '\n';
     const signedHeaders = 'content-type;host';
     const payloadHash = sha256(body);
     const canonicalRequest = 'POST\n/\n\n' + canonicalHeaders + '\n' + signedHeaders + '\n' + payloadHash;
 
-    // 待签字符串
     const algorithm = 'TC3-HMAC-SHA256';
     const credentialScope = date + '/' + service + '/tc3_request';
     const stringToSign = algorithm + '\n' + timestamp + '\n' + credentialScope + '\n' + sha256(canonicalRequest);
 
-    // 派生密钥
     const kDate = binaryHmacSha256('TC3' + secretKey, date);
     const kService = binaryHmacSha256(kDate, service);
     const kSigning = binaryHmacSha256(kService, 'tc3_request');
@@ -73,7 +226,6 @@ if (typeof $request !== 'undefined') {
 
 // ==================== 定时签到 ====================
 (async () => {
-    // 读取密钥
     const secretRaw = $.getdata(SECRET_KEY_KEY) || '';
     const [SECRET_ID, SECRET_KEY] = secretRaw ? secretRaw.split(':') : ['', ''];
     if (!SECRET_ID || !SECRET_KEY) {
@@ -148,7 +300,6 @@ if (typeof $request !== 'undefined') {
     $.done();
 })();
 
-// ==================== 网络请求 ====================
 function doPost(url, body, authorization) {
     return new Promise((resolve, reject) => {
         $.post({
