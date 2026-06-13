d1dfc655e201e784f957c70e4818be383586dca0
littleanzi <duanyaan@outlook.com>
2026-06-14 01:40:04 +0800

    1.1.3

--- script/naixue.js
+++ script/naixue.js
@@ -1,6 +1,6 @@
 /**
 * 奈雪点单·签到脚本
-* 2026-06-14 版本: 1.1.2
+* 2026-06-14 版本: 1.1.3
 * 签名密钥 (HmacSHA1): sArMTldQ9tqU19XIRDMWz7BO5WaeBnrezA
 * MITM 域名: tm-api.pin-dao.cn
 * 重写规则 (Rewrite): ^https://tm-api\.pin-dao\.cn/passport/authenticate/wxapp/verify/grc url script-response-body naixue.js
@@ -97,44 +97,20 @@ function hmac_sha1(msg, key) {
 }
 
 // ====== Base64 ======
-function base64Encode(input) {
+function hexToBytes(hex) {
+    var bytes = [];
+    for (var i = 0; i < hex.length; i += 2) bytes.push(parseInt(hex.substr(i, 2), 16));
+    return bytes;
+}
+
+function bytesToBase64(bytes) {
     var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
     var output = "", chr1, chr2, chr3, enc1, enc2, enc3, enc4;
     var i = 0;
-    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-    while (i < input.length) {
-        enc1 = _keyStr.indexOf(input.charAt(i++)); enc2 = _keyStr.indexOf(input.charAt(i++));
-        enc3 = _keyStr.indexOf(input.charAt(i++)); enc4 = _keyStr.indexOf(input.charAt(i++));
-        chr1 = (enc1 << 2) | (enc2 >> 4); chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-        chr3 = ((enc3 & 3) << 6) | enc4;
-        output += String.fromCharCode(chr1);
-        if (enc3 != 64) output += String.fromCharCode(chr2);
-        if (enc4 != 64) output += String.fromCharCode(chr3);
-    }
-    return output;
-}
-
-function utf8Encode(string) {
-    string = string.replace(/\r\n/g, "\n");
-    var utftext = "";
-    for (var n = 0; n < string.length; n++) {
-        var c = string.charCodeAt(n);
-        if (c < 128) utftext += String.fromCharCode(c);
-        else if ((c > 127) && (c < 2048)) { utftext += String.fromCharCode((c >> 6) | 192); utftext += String.fromCharCode((c & 63) | 128); }
-        else { utftext += String.fromCharCode((c >> 12) | 224); utftext += String.fromCharCode(((c >> 6) & 63) | 128); utftext += String.fromCharCode((c & 63) | 128); }
-    }
-    return utftext;
-}
-
-function b64Encode(str) {
-    var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-    var output = "", chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-    var i = 0;
-    str = utf8Encode(str);
-    while (i < str.length) {
-        chr1 = str.charCodeAt(i++); chr2 = str.charCodeAt(i++); chr3 = str.charCodeAt(i++);
+    while (i < bytes.length) {
+        chr1 = bytes[i++]; chr2 = i < bytes.length ? bytes[i++] : 0; chr3 = i < bytes.length ? bytes[i++] : 0;
         enc1 = chr1 >> 2; enc2 = ((chr1 & 3) << 4) | (chr2 >> 4); enc3 = ((chr2 & 15) << 2) | (chr3 >> 6); enc4 = chr3 & 63;
-        if (isNaN(chr2)) { enc3 = enc4 = 64; } else if (isNaN(chr3)) { enc4 = 64; }
+        if (i - 1 > bytes.length) { enc3 = enc4 = 64; } else if (i - 2 > bytes.length) { enc4 = 64; }
         output += _keyStr.charAt(enc1) + _keyStr.charAt(enc2) + _keyStr.charAt(enc3) + _keyStr.charAt(enc4);
     }
     return output;
@@ -143,7 +119,8 @@ function b64Encode(str) {
 // ====== 签名算法 ======
 function generateSignature(nonce, openId, timestamp) {
     var data = "nonce=" + nonce + "&openId=" + openId + "&timestamp=" + timestamp;
-    return b64Encode(hmac_sha1(data, CONFIG.signKey));
+    var hmacHex = HmacSHA1(data, CONFIG.signKey);
+    return bytesToBase64(hexToBytes(hmacHex));
 }
 
 // ====== 请求体构建 ======
